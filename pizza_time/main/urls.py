921d13b7cd49522618f068e76ccb0faeea17daa8
Keith Peterson <keithpe@mac.com>
2020-07-18 16:48:32 -0600

    Add and renamed images for quick options page. Modify/clean up urls. Add order process and checkout stub view methods.

--- pizza_time/main/urls.py
+++ pizza_time/main/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('quick/', views.index),
     path('order/', views.order),
+    path('order/favorite/', views.order_favorite),
+    path('order/surprise/', views.order_surprise),
+    path('order/process/', views.order_process),
+    path('order/checkout/', views.order_checkout),
 ]
--- pizza_time/main/views.py
+++ pizza_time/main/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render, redirect, HttpResponse
 
 
 def index(request):
+    print('Quick Options')
     return render(request, 'index.html')
 
 
 def order(request):
+    print('Order selected')
     return render(request, 'order.html')
+
+
+def order_favorite(request):
+    print('Order FAVORITE selected')
+    return render(request, 'order.html')
+
+
+def order_surprise(request):
+    print('Order SURPRISE selected')
+    return render(request, 'order.html')
+
+
+def order_process(request):
+    print('Process order')
+    # Process the order and
+    return render(request, 'checkout.html')
+
+
+def order_checkout(request):
+    print('Checkout')
+    return render(request, 'checkout.html')
